8e8cf4728a75db82f8688dd3b1549d8f3150d77f
Kazi Naimul Hoque <90389165+aoyan107@users.noreply.github.com>
2022-10-21 20:14:40 +0600

    Update rootSocket.ts

--- src/config/rootSocket.ts
+++ src/config/rootSocket.ts
@@ -3,13 +3,8 @@ import * as socketIo from 'socket.io';
 export const rootSocket = (io: socketIo.Server) => {
     io.on('connection', (socket) => {
         console.log('New connection');
-        socket.on('join-location-room', (room) => {
-            console.log('join room for Location', room);
-            socket.join(room);
-        });
-
-        socket.on('join-grid-room', (room) => {
-            console.log('join room for Grid', room);
+        socket.on('join-new-room', (room) => {
+            console.log('join new room', room);
             socket.join(room);
         });
 
