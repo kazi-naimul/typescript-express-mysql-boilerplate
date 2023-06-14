1976455720de6abf8a9a5b11ee7089fba08dc503
Kazi Naimul Hoque <90389165+aoyan107@users.noreply.github.com>
2023-06-14 10:53:40 +0600

    Update redisClient.ts

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -3,10 +3,10 @@ import * as Redis from 'redis';
 import { config } from './config';
 
 const url = `redis://${config.redis.host}:${config.redis.port}`;
-const client: Redis.RedisClientType = Redis.createClient({ url });
+const redisClient: Redis.RedisClientType = Redis.createClient({ url });
 if (config.redis.usePassword.toUpperCase() === 'YES') {
-    client.auth(config.redis.password);
+    redisClient.auth(config.redis.password);
 }
 
 console.log('Redis Client loaded!!!');
-module.exports = client;
+module.exports = redisClient;
